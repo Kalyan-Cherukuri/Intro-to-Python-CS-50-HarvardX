96ea98af9e1e674fb3ad3c521c3dce0c91dbb076
CHERUKURI KALYAN <svnkalyan@gmail.com>
2024-11-25 13:06:11 -0600

    Deep.py

---
+++ week-2/deep.py
@@ -0,0 +1,7 @@
+str = input("What is the answer to the Great Question of Life, the Universe and Everything?")
+
+if(str=="42") or (str=="forty-two") or (str=="forty two"):
+    print("Yes")
+
+else:
+    print("No")
