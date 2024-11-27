4acb314eb798742de12443b706887f501fa8ee77
Kalyan Cherukuri <kalyancherukuri@Kalyans-MacBook-Pro.local>
2024-11-27 12:24:48 -0600

    fuel.py

--- week-3/fuel.py
+++ week-3/fuel.py
@@ -1,23 +1,19 @@
-str1 = input("Fraction: ")
-
-x, y = str1.split("/")
-
-Fuel = (int(x)/int(y))*100
-
-
-if Fuel == 0:
-    print("E")
-if (Fuel > 0) and (Fuel <= 25):
-    print("25%")
-if (Fuel > 25) and (Fuel <= 50):
-    print("50%")
-if (Fuel > 50) and (Fuel <= 75):
-    print("75%")
-if (Fuel > 75) and (Fuel < 100):
-    print("100%")
-if Fuel == 100:
-    print("F")
-elif int(x) == 0:
-    print("")
-else:
-    print("")
+while(True):
+    try:
+        str1 = input("Fraction: ")
+        x, y = str1.split("/")
+        Fuel = (int(x) / int(y)) * 100
+        if (Fuel>=0) and (Fuel<20):
+            print("E")
+        elif (Fuel>=20) and (Fuel<40):
+            print("25%")
+        elif (Fuel>=40) and (Fuel<60):
+            print("50%")
+        elif (Fuel>=60) and (Fuel<80):
+            print("75%")
+        elif (Fuel>=80) and (Fuel<=100):
+            print("F")
+    except (ZeroDivisionError, ValueError):
+        print("X is not an integer")
+    else:
+        break
